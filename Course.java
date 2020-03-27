ce02f11d900512c77362d548176beeca4aa444c9
Gajeju <45118610+Gajeju@users.noreply.github.com>
2020-03-27 20:11:33 +0900

    이클립스 실습

---
+++ Course.java
@@ -0,0 +1,23 @@
+package course;
+
+//2015108223 전자공학과 김동현
+
+public class Course {
+
+	public static void main(String[] args) {
+		
+		System.out.println("시스템설계 대학원 졸업 후 전공관련 직종 취업");
+	}
+
+	class A{
+		
+	}
+}
+
+class B{
+	
+}
+
+class C{
+	
+}
