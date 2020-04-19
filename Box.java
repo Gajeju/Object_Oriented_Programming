19ccf6b7e9bf4988695f1c6b6ff3ed883bdc9af2
Gajeju <45118610+Gajeju@users.noreply.github.com>
2020-04-19 16:32:25 +0900

    객체생성, this, this()

---
+++ Box.java
@@ -0,0 +1,40 @@
+package box;
+
+public class Box {
+	private int width, height;
+	private char fillchar;
+	
+	public Box() {			//10x1 박스
+		this(10,1);
+	}
+	
+	public Box(int width, int height) {		//너비, 높이 가진 생성자
+		this.width = width;
+		this.height = height;
+	}
+	
+	public void draw() {		//박스를 그리는 메소드
+		for (int i = 0; i < height; i++)
+		{
+				for (int j = 0; j < width; j++) {
+					System.out.print(fillchar);
+				}
+				System.out.println("");
+		}
+	}
+	
+	public void fill(char c) {		//문자 설정
+		fillchar = c;
+	}
+	
+	public static void main(String[] args) {
+		Box a = new Box();
+		Box b = new Box(20,3);
+		a.fill('*');
+		b.fill('%');
+		a.draw();
+		b.draw();
+
+	}
+
+}
